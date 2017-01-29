46c0fef20b9ecdb0ddc4e7a9dd05686ea6a0d33b
Christian Kniep <christian@qnib.org>
2017-01-29 19:19:14 +0100

    rearrange file for Docker image

---
+++ apisender/start_stack.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+docker-compose ps geth >>/dev/null
+WAIT=0
+if [ $? -ne 0 ]; then
+    WAIT=1
+    echo "[II] Start geth"
+    docker-compose up -d geth
+fi
+docker-compose ps apireceiver >>/dev/null
+if [ $? -ne 0 ]; then
+    echo "[II] Start apireceiver"
+    docker-compose up -d apireceiver
+    WAIT=1
+fi
+if [ ${WAIT} -eq 1 ];then
+    echo "[II] Wait for dependencies to settle"
+    sleep 10
+fi
+docker-compose up apisender
+docker rm -f
