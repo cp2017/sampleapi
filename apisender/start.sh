27bfd9012a0317d882e2f61c35476262c97577f3
Christian Kniep <christian@qnib.org>
2017-01-29 18:48:38 +0100

    Successfull test
    - updated cp2017sign to latest version
    - left ethAddress blank
    - add files to start stack

--- apisender/start.sh
+++ apisender/start.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-if [ ! -d ${ETH_DATADIR} ];then
-    echo "[EE] Missing data directory '${ETH_DATADIR}'"
-    exit 1
-else
-    echo "[II] Content of '${ETH_DATADIR}'"
-    find ${ETH_DATADIR} -type f
+docker-compose ps geth >>/dev/null
+WAIT=0
+if [ $? -ne 0 ]; then
+    WAIT=1
+    echo "[II] Start geth"
+    docker-compose up -d geth
 fi
-
-mv server.js server.js.orig
-set -x
-cat server.js.orig \
-  |sed -e "s#ethAddress = ''.*#ethAddress = '${ETH_ADDR}';#" \
-  |sed -e "s#ethDataDir = ''.*#ethDataDir = '${ETH_DATADIR}';#" \
-  |sed -e "s#ethPassword = ''.*#ethPassword = '${ETH_PW}';#" > server.js
-
-ln -s ${ETH_DATADIR} ${HOME}/.ethereum
-npm start
-if [ "X${HANG_IN_THERE}" == "Xtrue" ];then
-    tail -f /dev/null
+docker-compose ps apireceiver >>/dev/null
+if [ $? -ne 0 ]; then
+    echo "[II] Start apireceiver"
+    docker-compose up -d apireceiver
+    WAIT=1
 fi
+if [ ${WAIT} -eq 1 ];then
+    echo "[II] Wait for dependencies to settle"
+    sleep 10
+fi
+docker-compose up apisender
+docker rm -f
